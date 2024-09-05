26e235fdad9a195a241630b6bc22947eb671c011
jennifer hodge <jennifer.hodge@russdarrow.com>
2024-09-04 19:45:01 -0500

    done, have to do explanation video

--- closest.ts
+++ closest.ts
@@ -1,6 +1,27 @@
-export function closest(nums: number[], target: number): number {
-   // Implement me
-   return 0;
-}
+// sort
+//loop
+// 2 pointers to find closest sum
 
-module.exports = { closest }
+export function closest(nums: number[], target: number): number {
+  nums.sort((a, b) => a - b);
+  let ans = nums[0] + nums[1] + nums[2];
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    let start = i + 1;
+    let end = nums.length - 1;
+    nums.length - 1;
+    while (start < end) {
+      const sum = nums[i] + nums[start] + nums[end];
+      if (sum > target) {
+        end -= 1;
+      } else {
+        start += 1;
+      }
+      if (Math.abs(target - sum) < Math.abs(target - ans)) {
+        ans = sum;
+      }
+    }
+  }
+  return ans;
+}
+module.exports = { closest };
